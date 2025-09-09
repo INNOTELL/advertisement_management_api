b1f7eca6fb4dc8b1b15516b4f7cb1e8cb3e80cbf
ezekiel <ezekdonkor@gmail.com>
2025-09-09 22:10:32 +0000

    I have connected to database and all is working except delete

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Form, File, UploadFile, status
 from pydantic import BaseModel
-from bson.objectid import ObjectId
+from db import advert_collection
+# from bson.objectid import ObjectId
 from typing import Annotated
+from utils import replace_mongo_id
 import cloudinary
 import cloudinary.uploader
+
+
+cloudinary.config(
+    cloud_name = "dyhqmkyfc",
+    api_key = "617624249245792",
+    api_secret = "cAhgM5MehvpHZu6wgW23h63n9WM"
+)
 
 
 app = FastAPI()
@@ -14,7 +23,7 @@ class NewAdvert(BaseModel):
     Price: float
     Category: str
 # creates a list to store posted ads
-advert = []
+# advert = []
 
 # creates an endpoint to the homepage
 @app.get("/")
@@ -22,42 +31,70 @@ def root():
     return{"Message":"Welcome to our Advertisement Management API"}
 
 # allows vendors to create a new advert.
-@app.post("/post_advert")
-def new_advert(new_advert: NewAdvert):
-    advert.append(new_advert.model_dump())
+@app.post("/advert")
+def new_advert(
+    Title: Annotated[str, Form()], 
+    Description: Annotated[str, Form()], 
+    Price: Annotated[float, Form()],
+    flyer:Annotated[UploadFile, File()]):
+
+    # upload flyer to cloudinary to get a url
+    upload_advert = cloudinary.uploader.upload(flyer.file)
+
+    advert_collection.insert_one({
+        "title": Title,
+        "Description": Description,
+        "price": Price,
+        "flyer": upload_advert["secure_url"]
+    })
     return{"message": "Your advert has been posted sucessfully!"}
 
 # allows vendors to view all adverts.
-@app.get("/all_adverts")
-def all_adverts():
-    return{"all_adverts":advert}
+@app.get("/adverts")
+def all_adverts(Title= "", description="", limit = 10, skip = 0):
+    advert = advert_collection.find(limit = int(limit), skip = int(skip)).to_list()
+    return {"data": list(map(replace_mongo_id, advert))}
+
+# def all_adverts():
+#     return{"all_adverts":{advert_collection}}
 
 # allows vendors to view a specific advert’s details
 @app.get("/advert_details/{Title}")
 def advert_details(Title:str):
-    for ads in advert:
-        if ads["Title"] == Title:
-            return ads
-        raise HTTPException(status_code=404, detail="Advert not found")
+    adverts = advert_collection.find_one({"title":Title})
+    if not adverts:
+        raise HTTPException(status.HTTP_422_UNPROCESSABLE_ENTITY, "Sorry advert not found😞")
+    return {"data": [replace_mongo_id(adverts)]}
+
+    # for adverts in advert_collection:
+    #     if adverts["Title"] == Title:
+    #         return ads
+    #     raise HTTPException(status_code=404, detail="Advert not found")
     
 # allows vendors to edit an advert
-@app.put("/edit_advert/{Title}")
-def advert_edit(Title,Description,Price,Category):
-    if not ObjectId.is_valid(Title):
-         raise HTTPException(status_code=404, detail="Advert not found")
-    
+@app.put("/edit_advert/{title}")
+def advert_edit(
+    Title: Annotated[str, Form()], 
+    Description: Annotated[str, Form()], 
+    Price: Annotated[float, Form()],
+    flyer:Annotated[UploadFile, File()]):
+    adverts = advert_collection.find_one({"title":Title})
+    if not adverts:
+        raise HTTPException(status_code=404, detail="Sorry advert not found😞")
+    uploald_advert = cloudinary.uploader.upload(flyer.file)
+    advert_collection.replace_one({"title": Title}, replacement= {
+        "Title": Title,
+        "Description": Description,
+        "price": Price,
+        "Flyer": uploald_advert["secure_url"]
+    })
+    return{"message": "You have successfully updated your Add✅"}
 
 # allows vendors to remove an advert
-@app.delete("/adverts/{Title}")
-def delete(Title):
-    if not ObjectId.is_valid(Title):
-        raise HTTPException(status_code=404, detail="Advert not found")
-    advert.delete_one(filter={"Title": ObjectId(Title)})
+@app.delete("/adverts/{title}")
+def delete_advert(Title):
+    adverts = advert_collection.find_one({"title":Title})
+    if not adverts:
+        raise HTTPException(status_code=404, detail="Sorry advert not found to be deleted😞")
+    advert_collection.delete_one({"title":Title})
     return{"message": "Advert successfully deleted!"}
-
-    
-
-
-        
-        
-
